e2f9d5dc20d051ed451f1de5811a030581f027c6
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2024-12-09 08:07:04 +0300

    ClassWork queries

---
+++ SQL/Functions/SQLQuery0 - CHECK Holidays.sql
@@ -0,0 +1,14 @@
+USE PD_318_DML
+GO
+
+SET DATEFIRST 1;
+--DELETE FROM DaysOFF;
+
+EXEC sp_NewYearDaysFor 2024;
+EXEC sp_MayHolidaysFor 2024;
+
+SELECT
+	[date],
+	DATENAME(WEEKDAY, [date]),
+	holiday_name
+FROM DaysOFF JOIN Holidays ON(holiday = holiday_id)
